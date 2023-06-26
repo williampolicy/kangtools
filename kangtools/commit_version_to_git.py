df036e2fbf8cbf78c4d70002b589fbe6fb0c0059
Willaimcode <kangxiaowen@gmail.com>
2023-06-26 11:06:11 -0400

    V.0.6 - test help  ©KANG - 2023-06-26

--- kangtools/commit_version_to_git.py
+++ kangtools/commit_version_to_git.py
@@ -1,49 +1,44 @@
 #!/usr/bin/env python3
-
 import os
 import re
 import datetime
+import argparse
 
-def main():
+def main(args):
     version_file_path = "version.txt"
-    commit_message = input("Enter commit message: ")
 
-    # Check if version.txt exists, create it if it doesn't
+    if args.commit_message:
+        commit_message = args.commit_message
+    else:
+        commit_message = input("Enter commit message: ")
+
     if not os.path.exists(version_file_path):
         with open(version_file_path, 'w') as f:
             f.write("V.0.1\n")
 
-    # Read previous version and commit history
     with open(version_file_path, 'r') as f:
         content = f.readlines()
-        # Initialize version as default
         version = "V.0.1"
-        # Start from the last line and move upwards
         for line in reversed(content):
-            # If this line looks like a version number, use it and stop
             if line.strip().startswith("V.0."):
                 version = line.strip()
                 break
 
-    # Extract version number
     version_number = int(re.search(r'\d+(?=\s|$)', version).group())
-
-    # Increment version number
     new_version_number = version_number + 1
-
-    # Get current timestamp
     timestamp = datetime.datetime.now().strftime('%Y-%m-%d')
 
-    # Update version file
     with open(version_file_path, 'a') as f:
-        f.write(f"V.0.{new_version_number} - {commit_message} ©KANG - {timestamp}\n")
+        f.write(f"V.0.{new_version_number} - {commit_message}  ©KANG - {timestamp}  \n")
 
-    # Execute git commands
     os.system("git add -A")
-    os.system(f'git commit -m "V.0.{new_version_number} - {commit_message} ©KANG - {timestamp}"')
+    os.system(f'git commit -m "V.0.{new_version_number} - {commit_message}  ©KANG - {timestamp} "')
     os.system("git push")
 
     print(f"Committed Version V.0.{new_version_number}")
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="This script handles versioning and git commit operations. The version number is stored in a text file, incremented, and then a new entry with the version number, a user-provided commit message, the user name, and the current timestamp is appended. These details are also used in the git commit message.")
+    parser.add_argument('-m', '--commit_message', type=str, help='Specify the commit message that will be used in both the version text file and the git commit. If not provided, the script will prompt for it.')
+    args = parser.parse_args()
+    main(args)
